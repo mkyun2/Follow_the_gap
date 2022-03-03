50780c4a4683e209cad7b7c118c52e426be3bbeb
mkyun2 <robotics.gyun@gmail.com>
2022-03-03 22:14:30 +0900

    Test 0.8v

--- src/follow_gap.cpp
+++ src/follow_gap.cpp
@@ -1,5 +1,7 @@
-#include<vector>
-#include<tf/tf.h>
+#include <iostream>
+#include <vector>
+
+#include <tf/tf.h>
 #include <ros/ros.h>
 #include <Eigen/Core>
 #include <geometry_msgs/PoseStamped.h>
@@ -8,14 +10,12 @@
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 #include "sensor_msgs/LaserScan.h"
-#include <sensor_msgs/PointCloud.h>
+//#include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/Imu.h>
 #include <signal.h>
-#include <memory.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
 using namespace std;
-using namespace Eigen;
 void handling(sig_atomic_t)
 {
     exit(1);
@@ -26,14 +26,14 @@ mavros_msgs::State current_state;
 sensor_msgs::LaserScan ds_data;
 geometry_msgs::PoseStamped LocalPosition;
 sensor_msgs::Imu imu_data;
-double roll, pitch, yaw;
+double roll, pitch, yaw = 0;
 //===================Obstacle avoidance Variable===================
 const int Max_distance=6000;
 const int smallestDistanceSaturation=1500;
 const double desired_eta = 100;
-const uint8_t num_readings = 120;
-uint8_t laser_frequency = 20;
-float ranges2[num_readings];
+const int num_readings = 120;
+const int laser_frequency = 20;
+float ranges2[num_readings]={0.0};
 float Final_Angle=0;
 //=================================================================
 void state_callback(const mavros_msgs::State::ConstPtr& msg){
@@ -62,6 +62,8 @@ void Lidar_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
 // 11 : obstacle area
     vector<int> Clearance_StartAngle;
     vector<int> Clearance_EndAngle;
+    vector<int> Obstacle_StartAngle;
+    vector<int> Obstacle_EndAngle;
     uint8_t Obstacle_flag = 0;
     uint8_t Obstacle_flag_pre = 0;
     uint8_t Num_of_Obstacle=0;
@@ -78,19 +80,31 @@ void Lidar_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
     int Num_of_obstacle_point[120] = {0};
     int Distance_of_Obstacle[120]  = {0};
 
-    int Distance_of_Obstacle_start[20] = {0};
-    int Distance_of_Obstacle_end[20]   = {0};
+    // int Distance_of_Obstacle_start[20] = {0};
+    // int Distance_of_Obstacle_end[20]   = {0};
 
-    int Clearance_point_start[120]  	= {0};
-    int Clearance_point_end[120]    	= {0};
+    int Distance_of_Clearance[120]  = {6000};
+    // int Clearance_point_start[120]  	= {0};
+    // int Clearance_point_end[120]    	= {0};
     int Num_of_Clearance_point[120] 	= {0};
     int Distance_of_Clearance_start[120]= {0};
     int Distance_of_Clearance_end[120]	= {0};
     // flag를 이용해 Obstacles와 clearances를 구분하는 function.
-
+    for(int i = 0; i<num_readings; i++){
+        Distance_of_Obstacle[i]=6000;
+        if((msg->ranges[i]*1000)<10)
+            ranges2[i]=Max_distance;
+        else if((msg->ranges[i]*1000)>Max_distance)
+            ranges2[i]=Max_distance;
+        else
+            ranges2[i]=msg->ranges[i]*1000;
+    }
+    
     for(int i=0; i<num_readings; i++)
     {
-        ranges2[i]=(msg->ranges[i]*1000)-(int)(msg->ranges[i]*1000)%10;
+        ranges2[i]=ranges2[i]-(int)ranges2[i]%10;
+        
+        //ROS_INFO("%f",ranges2[i]);
         if(ranges2[i] < Max_distance)
         {
             Obstacle_flag = 1;
@@ -104,22 +118,30 @@ void Lidar_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
             Obstacle_flag = 0;
             Clearance_flag = 1;
         }
+        
         //======================Obstacle=============================
         if(Obstacle_flag==1 && Obstacle_flag_pre==0)
         {
             Num_of_Obstacle++;
         }
+        
         Obstacle_flag_pre=Obstacle_flag;
         //======================Clearance=============================
         //첫 시작이 clearance라면 
         if(Clearance_flag == 1 && Clearance_flag_pre == 0)
             Num_of_Clearance++;
         Clearance_flag_pre=Clearance_flag;
-    }
+        
+    
+    Obstacle_flag=0;
+    Obstacle_flag_pre=0;}
+    Clearance_flag=0;
+    Clearance_flag_pre=0;	
 //========================Obstacle,Clearance info =============================
     for(int i=0; i<num_readings; i++)
     {
-        uint8_t Obstacle_Points = 0;
+        
+        
         if(ranges2[i] < Max_distance)
         {
             Obstacle_flag = 1;
@@ -132,78 +154,157 @@ void Lidar_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
         }
         if((Obstacle_flag == 1) && (Obstacle_flag_pre == 0))
         {
-            
-            Obstacle_point_start[Obstacle_Points] = i; // start point pos
-            Distance_of_Obstacle_start[Obstacle_Points]=ranges2[i]; // distance to start point
-            Num_of_obstacle_point[Obstacle_Points] = Num_of_obstacle_point[Obstacle_Points] + 1; //
+            // ROS_INFO("Obstacle Start, %d", i);
+            // ROS_INFO("Obstacle StartPoint: %d",i);
+            //Obstacles_StartAngle.push_back(i);
+            //Obstacle_point_start[i] = i; // start point pos
+            Distance_of_Obstacle[i]=ranges2[i]; // distance to start point
+            Num_of_obstacle_point[i] = Num_of_obstacle_point[i] + 1; //
 
         }
         else if((Obstacle_flag == 1) && (Obstacle_flag_pre == 1))
         {
-            Num_of_obstacle_point[Obstacle_Points] = Num_of_obstacle_point[Obstacle_Points] + 1; // Obstacle을 구성하는 Points의 개수를 counting
+            // ROS_INFO("Obstacle ing");
+            Num_of_obstacle_point[i] = Num_of_obstacle_point[i] + 1; // Obstacle을 구성하는 Points의 개수를 counting
             if(i > 118)
             {
-                
-                Obstacle_point_end[Obstacle_Points] = i;// * 0.25 - 60;	// Obstacle Ending Point
-                Distance_of_Obstacle_end[Obstacle_Points] = ds_data.ranges[i];///////////////////////////////////////////////////////
-                Obstacle_Points++;	// Obstacle_point : the number of obstacle points
+                // ROS_INFO("Obstacle EndPoint: %d",i);
+                //Obstacle_point_end[i] = i;// * 0.25 - 60;	// Obstacle Ending Point
+                Distance_of_Obstacle[i] = ranges2[i];///////////////////////////////////////////////////////
+                //i++;	// Obstacle_point : the number of obstacle points
             }
         }
         else if((Obstacle_flag == 0) && (Obstacle_flag_pre == 1))
         {
+            //Obstacle_EndAngle.push_back(i);
+            // ROS_INFO("Obstacle End, %d",i);
+            // ROS_INFO("Obstacle End Clearance Flag=%d, %d",Clearance_flag,Clearance_flag_pre);
             
-            Obstacle_point_end[Obstacle_Points] = i-1;// * 0.25 - 60;	// Obstacle Ending Point
-            Distance_of_Obstacle_end[Obstacle_Points] = ranges2[i-1];///////////////////////////////////////////////////////
-            Obstacle_Points++;
+            //Obstacle_point_end[i] = i;// * 0.25 - 60;	// Obstacle Ending Point
+            Distance_of_Obstacle[i] = ranges2[i-1];///////////////////////////////////////////////////////
+            //i++;
         }
-        else if((Clearance_flag == 1) && (Clearance_flag_pre == 0))
+        // ROS_INFO("Distance Obstacle, %d, %d, %f",Distance_of_Obstacle[i],i,ranges2[i]);
+        // // else
+        // {
+        //     i = i;
+        // }
+        if((Clearance_flag == 1) && (Clearance_flag_pre == 0))
         {
-            Clearance_point_start[Obstacle_Points] = i;
+            // ROS_INFO("Clearance Start, %d", i);
+            // ROS_INFO("Clear startPoint: %d",i);
+            //Clearance_point_start[i] = i;
             Clearance_StartAngle.push_back(i);
+        }
+        else if((Clearance_flag == 1) && (Clearance_flag_pre == 1))
+        {
+            // ROS_INFO("Clearance ing");
+        //	Obstacle_Area[Num_of_obstacle][Num_of_obstacle_point[Num_of_obstacle]] = ds_data.ranges[i];
+        //	Obstacle_Area[1][Num_of_obstacle][Num_of_obstacle_point[Num_of_obstacle]] = i;
+            Num_of_Clearance_point[i] = Num_of_Clearance_point[i] + 1;
+            if(i > 118)
+            {
+                // ROS_INFO("Clearance End1, %d, Ranges:%f", i,ranges2[i]);
+                // ROS_INFO("Clear EndPoint: %d",i);
+                Clearance_EndAngle.push_back(i);
+                //Clearance_point_end[i] = i;// * 0.25 - 60;	// Obstacle Ending Point
+                Distance_of_Clearance[i] = ranges2[i];
+                //i++;
+            }
+        //	Num_of_obstacle_point[Num_of_obstacle] = Num_of_obstacle_point[Num_of_obstacle] + 1;
         }
         else if((Clearance_flag == 0) && (Clearance_flag_pre == 1))
         {
-            Clearance_point_end[Obstacle_Points] = i;
+            // ROS_INFO("Clearance End2, %d", i);
+            // //Clearance_point_end[i] = i;
+            // ROS_INFO("Clear EndPoint: %d",i);
+            Distance_of_Clearance[i] = ranges2[i];
             Clearance_EndAngle.push_back(i);
+            //i++;
         }
-        else
-        {
-            Obstacle_Points = Obstacle_Points;
-        }
+        // else
+        // {
+        //     i = i;
+        // }
         Obstacle_flag_pre = Obstacle_flag;
+        Clearance_flag_pre = Clearance_flag;
         //Known Obstacle Num, Angle, distance, points
-        
+        ROS_INFO("Clear: %d",Num_of_Clearance);
     }
     //start num --> Vector(angle num)
     //Angle of obstacle and cleanrance
     //Clearance num + Obstacle num 
     vector<int> GapAngle;
     vector<double> FinalAngle;
+    vector<double> MinDistance;
     double Now_FinalAngle=0.0;
     double Max_FinalAngle=0.0;
-    int Now_GapAngle=0;
+    double Now_GapAngle=0;
+    double Max_GapAngle = 0;
     int Max_GapAngle_Num=0;
-    int Min_Distance=0;
-    float Gain_GapAngle = 1;
-    float Gain_GoalAngle=1;
-    float GoalAngle=0;
-    for(int i=0; i<Clearance_StartAngle.size(); i++)
-    {
-        if(Distance_of_Clearance_end[Clearance_EndAngle[i]] > Distance_of_Clearance_start[Clearance_StartAngle[i]])
-            Min_Distance = Distance_of_Clearance_start[Clearance_StartAngle[i]];
+    float Min_Distance=6.0;
+    float Now_Distance=0;
+    float Gain_GapAngle = 5;
+    float Gain_GoalAngle= 1;
+    float GoalAngle=10;
+    ROS_INFO("size: %d",Clearance_StartAngle.size());
+     for(int i=0; i<Clearance_StartAngle.size(); i++)
+     {  
+        ROS_INFO("CHecking Clearance_EndDistance: %d, StartDistance: %d",Distance_of_Obstacle[Clearance_EndAngle[i]],Distance_of_Obstacle[Clearance_StartAngle[i]]);
+            
+        if(Distance_of_Obstacle[Clearance_EndAngle[i]] < Distance_of_Obstacle[Clearance_StartAngle[i]])
+            {
+                // if(Clearance_EndAngle[i]==num_readings-1)
+                //         Min_Distance = ranges2[Clearance_EndAngle[i]];
+                // else
+                MinDistance.push_back(Distance_of_Obstacle[Clearance_EndAngle[i]]);
+                //Min_Distance = Distance_of_Obstacle[Clearance_EndAngle[i]];
+                //ROS_INFO("CHecking2 Clearance_EndAngle: %d, StartAngle: %d",Clearance_EndAngle[i],Clearance_StartAngle[i]);
+                //ROS_INFO("CHecking2 Clearance_EndDistance: %d, StartDistance: %d",Distance_of_Obstacle[Clearance_EndAngle[i]],Distance_of_Obstacle[Clearance_StartAngle[i]]);
+            
+            }
         else
-            Min_Distance = Distance_of_Clearance_end[Clearance_EndAngle[i]]; // how do robot when approach to zero 
-        Now_GapAngle = Clearance_EndAngle[i] - Clearance_StartAngle[i];
+            {
+                // if(Clearance_StartAngle[i]==0)
+                
+                //     Min_Distance = ranges2[Clearance_StartAngle[i]]; // how do robot when approach to zero 
+                // else
+                MinDistance.push_back(Distance_of_Obstacle[Clearance_StartAngle[i]]);
+                //Min_Distance = Distance_of_Obstacle[Clearance_StartAngle[i]]; // how do robot when approach to zero 
+                //ROS_INFO("CHecking1 Clearance_EndAngle: %d, StartAngle: %d",Clearance_EndAngle[i],Clearance_StartAngle[i]);
+                //ROS_INFO("CHecking1 Clearance_EndDistance: %d, StartDistance: %d",Distance_of_Obstacle[Clearance_EndAngle[i]],Distance_of_Obstacle[Clearance_StartAngle[i]]);
+            
+            }
+        
+        //if(Now_Distance < Min_Distance)
+            //Min_Distance=Now_Distance;
+        //Gap_CenterAngle
+        Now_GapAngle = (((Clearance_EndAngle[i]-60) - (Clearance_StartAngle[i]-60))/2);//+Clearance_StartAngle[i]-60;
+        
         GapAngle.push_back(Now_GapAngle);
-        Now_FinalAngle = (Gain_GapAngle/Min_Distance*Now_GapAngle+Gain_GoalAngle*GoalAngle)/(Gain_GapAngle/Min_Distance+Gain_GoalAngle);
-        FinalAngle.push_back(Now_FinalAngle);
-        if(Max_FinalAngle<Now_FinalAngle)
-            Max_FinalAngle = Now_FinalAngle;
+
+        ROS_INFO("Start Angle: %f, End Angle: %f",Clearance_StartAngle[i]-60+(yaw*180.0/M_PI),Clearance_EndAngle[i]-60+(yaw*180.0/M_PI));
+        
+        // FinalAngle.push_back(Now_FinalAngle);
+        if(abs(Max_GapAngle)<abs(Now_GapAngle))
+        {
+           Max_GapAngle = Now_GapAngle;
+        }
     }
-    if(find(FinalAngle.begin(),FinalAngle.end(), Max_FinalAngle) != FinalAngle.end())
-        Max_GapAngle_Num = find(FinalAngle.begin(), FinalAngle.end(), Max_FinalAngle) - FinalAngle.begin();
-    //}or SORT -> 0th index 
-    Final_Angle = FinalAngle[Max_GapAngle_Num];
+    if(find(GapAngle.begin(),GapAngle.end(), Max_GapAngle) != GapAngle.end())
+        Max_GapAngle_Num = find(GapAngle.begin(), GapAngle.end(), Max_GapAngle) - GapAngle.begin();
+    Min_Distance = MinDistance[Max_GapAngle_Num];
+    Min_Distance=Min_Distance/1000.0;
+    ROS_INFO("Min_Distance: %f",Min_Distance);
+    Max_GapAngle = GapAngle[Max_GapAngle_Num]+(Clearance_StartAngle[Max_GapAngle_Num]-60+(yaw*180.0/M_PI));
+    Final_Angle = ((Gain_GapAngle/Min_Distance*Max_GapAngle)+(Gain_GoalAngle*GoalAngle)) / ((Gain_GapAngle/Min_Distance)+Gain_GoalAngle);
+        
+    // if(find(FinalAngle.begin(),FinalAngle.end(), Max_FinalAngle) != FinalAngle.end())
+    //     Max_GapAngle_Num = find(FinalAngle.begin(), FinalAngle.end(), Max_FinalAngle) - FinalAngle.begin();
+    // // //}or SORT -> 0th index 
+    //Final_Angle = FinalAngle[Max_GapAngle_Num];
+    ROS_INFO("Gap Angle: %f, Min_Distance: %f",Max_GapAngle,Min_Distance);
+    ROS_INFO("GoalAngle: %f Final_Angle: %f Heading: %f", GoalAngle,Final_Angle, yaw*180.0/M_PI);
     //Known GapAngle Num, Max_GapAngle, obstacle distance
     
 
@@ -233,7 +334,7 @@ void Control_Robot()//차이 알아보기
     AttractiveForce(2) = PositionError(2);
     
     DesiredYaw=atan2(PositionError(1),PositionError(0));
-    YawError = DesiredYaw - yaw;
+    YawError = (Final_Angle - yaw);
     cmd_vel.linear.x = AttractiveForce(0);
     cmd_vel.linear.y = AttractiveForce(1);
     cmd_vel.linear.z = AttractiveForce(2);
@@ -275,6 +376,10 @@ int main(int argc, char **argv)
     
     geometry_msgs::PoseStamped pose;
     signal(SIGINT,handling);
+    while(ros::ok() && !current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
     while(ros::ok()){
         Control_Robot();
         Velocity_Publisher.publish(cmd_vel);
